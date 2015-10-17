6324c176d5d195241bcad280ff659c7eda4afa94
Jason Ngo <jasonngo@cs.utexas.edu>
2015-10-17 15:24:43 -0500

    flask directories

--- crimecast.py
+++ crimecast.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 #crimes page
 @app.route('/crimes')
 def splash():
-	return app.send_static_file('crimes.html')
+	return app.send_static_file('html/crimes.html')
 
 
 if __name__ == "__main__":
