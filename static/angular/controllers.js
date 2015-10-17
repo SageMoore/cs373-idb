81afc5a4e722187a5e41b2fdede7d7798f891cfd
Mark Daniel <mark.r.daniel@gmail.com>
2015-10-17 16:31:30 -0500

    pushing broken code

--- static/angular/controllers.js
+++ static/angular/controllers.js
@@ -1,4 +1,8 @@
 'use strict';
-crime_cast_app.controller('crime_cast_ctrl', function() {
+crime_cast_app.controller('crime_cast_ctrl', function($scope) {
+    console.log('controller is working')
 
+    $scope.go_to_link = function(link) {
+
+    }
 });
