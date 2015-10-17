c36f1e9d2915ce01d58afed1864870c6a353be79
Mark Daniel <mark.r.daniel@gmail.com>
2015-10-17 15:41:06 -0500

    adding changes and setting up angular

---
+++ webapp/js/app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by markdaniel on 10/17/15.
+ */
+var crime_cast_app = angular.module('crime_cast_app', []);
+
+crime_cast_app.config([]);
---
+++ webapp/js/controllers.js
@@ -0,0 +1,4 @@
+'use strict';
+crime_cast_app.controller('crime_cast_ctrl', function() {
+
+});
