ec1a08035e969a0822e4ab29b6bb8334a2b542fb
chance raine <chanceraine@gmail.com>
2015-10-17 16:40:17 -0500

    crimecast updated

--- crimecast.py
+++ crimecast.py
@@ -1,21 +1,36 @@
 from flask import Flask
+from flask import render_template
 
-app = Flask(__name__, static_url_path = "")
+app = Flask(__name__, static_url_path="")
 
 # Homepage
 @app.route('/')
 def splash():
-    return app.send_static_file('splash.html')
+    return render_template('splash.html')
+
 
 # Static crimes page
 @app.route('/crimes')
 def crimes():
-	return app.send_static_file('crimes.html')
+    return render_template('crimes.html')
+
 
 # Static weeks page
 @app.route('/weeks')
 def weeks():
-    return app.send_static_file('weeks.html')
+    return render_template('weeks.html')
+
+
+# Crime Type
+@app.route('/crimetype')
+def crimetype_home():
+    return app.send_static_file('crimeTypes.html')
+
+
+@app.route('/crimetype/<crimetype_id>')
+def crimetype(crimetype_id):
+    return app.send_static_file('crimeType_' + str(crimetype_id) + ' .html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
