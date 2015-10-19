6d4646335211d35a34e0aef85f7855f0260d37f8
Mark Daniel <mark.r.daniel@gmail.com>
2015-10-19 15:50:35 -0500

    moving everything to static & adding more css

--- static/angular/app.js
+++ static/angular/app.js
@@ -7,27 +7,27 @@ crime_cast_app.config(['$stateProvider', '$urlRouterProvider',
     function($stateProvider, $urlRouterProvider) {
         $stateProvider.state('splash', {
             url: '/splash',
-            templateUrl: '../public/splash.html',
+            templateUrl: 'splash.html',
             controller: 'crime_cast_ctrl'
         }).state('crimes', {
             url: '/crimes',
-            templateUrl: '../public/crimes.html',
+            templateUrl: 'crimes.html',
             controller: 'crime_cast_ctrl'
         }).state('weeks', {
             url: '/weeks',
-            templateUrl: '../public/weeks.html',
+            templateUrl: 'weeks.html',
             controller: 'crime_cast_ctrl'
         }).state('crime_types', {
             url: '/crime_types',
-            templateUrl: '../public/crime_types.html',
+            templateUrl: 'crime_types.html',
             controller: 'crime_cast_ctrl'
         }).state('zips', {
             url: '/zips',
-            templateUrl: '../public/zips.html',
+            templateUrl: 'zips.html',
             controller: 'crime_cast_ctrl'
         }).state('about', {
             url: '/about',
-            templateUrl: '../public/about.html',
+            templateUrl: 'about.html',
             controller: 'crime_cast_ctrl'
         })
         $urlRouterProvider.otherwise('splash');
