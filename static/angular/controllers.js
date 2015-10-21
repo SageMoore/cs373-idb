8ae8b8b8a61365897f03b242cbb348857d96f31a
Mark Daniel <mark.r.daniel@gmail.com>
2015-10-21 14:32:46 -0500

    adding gmaps and map of austin using npm

--- static/angular/controllers.js
+++ static/angular/controllers.js
@@ -5,5 +5,25 @@ crime_cast_app.controller('crime_cast_ctrl', function($scope) {
         //todo
     }
 
+    var get_map = function() {
+        try {
+            console.log('getting map')
+            var map = new GMaps({
+                el: '.gmap',
+                zoom: 11,
+                lat: 30.250000,
+                lng: -97.750000
+            });
+            console.log(map);
+        }
+        catch(err) {
+                console.log(err.message);
+            }
+    }
+
+    get_map();
+
+
+    $scope.get_map = get_map();
     $scope.go_to_link = go_to_link;
 });
