59ab6f69f9a3b971ba82db2fa445dff9de74073a
Mark Daniel <mark.r.daniel@gmail.com>
2015-10-21 15:20:30 -0500

    improving formatting of maps and cleaning stuff up

--- static/angular/controllers.js
+++ static/angular/controllers.js
@@ -7,14 +7,38 @@ crime_cast_app.controller('crime_cast_ctrl', function($scope) {
 
     var get_map = function() {
         try {
-            console.log('getting map')
             var map = new GMaps({
                 el: '.gmap',
-                zoom: 11,
-                lat: 30.250000,
-                lng: -97.750000
+                zoom: 12,
+                lat: 30.280000,
+                lng: -97.740000
             });
-            console.log(map);
+            map.addMarker({
+                lat: 30.30000,
+                lng: -97.730000,
+                title: 'Quacks',
+                click: function(e) {
+                    alert('Alert: Burglary at Quacks Bakery.');
+                }
+            });
+
+            map.addMarker({
+                lat: 30.27000,
+                lng: -97.7190000,
+                title: '12th and Chicon',
+                click: function(e) {
+                    alert('Alert: Murder at 12th and Chicon.');
+                }
+            });
+
+            map.addMarker({
+                lat: 30.28500,
+                lng: -97.7320000,
+                title: 'GDC',
+                click: function(e) {
+                    alert('Alert: Graffiti on GDC.');
+                }
+            });
         }
         catch(err) {
                 console.log(err.message);
