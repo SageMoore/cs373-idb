d49b55ed0f3ed66d006d5e4dd53136228538428a
dustinstone <stone.dustin94@gmail.com>
2015-10-21 19:23:22 -0500

    Adding unit tests and makefile

--- crimecast.py
+++ crimecast.py
@@ -12,19 +12,19 @@ def splash():
 # Static crimes page
 @app.route('/crimes')
 def crimes():
-    return render_template('crimes.html')
+    return app.send_static_file('crimes.html')
 
 
 # Static weeks page
 @app.route('/weeks')
 def weeks():
-    return render_template('weeks.html')
+    return app.send_static_file('weeks.html')
 
 
 # Crime Type
 @app.route('/crimetype')
 def crimetype_home():
-    return render_template('crimeType_1.html')
+    return app.send_static_file('crimeType_1.html')
 
 
 @app.route('/crimetype/<crimetype_id>')
@@ -32,15 +32,15 @@ def crimetype(crimetype_id):
     print(crimetype_id)
     type_file = 'crimeType_' + str(crimetype_id) + '.html'
     print(type_file)
-    return render_template(type_file)
+    return app.send_static_file(type_file)
 
 @app.route('/zips')
 def zip_home():
-    return render_template('zips.html')
+    return app.send_static_file('zips.html')
 
 @app.route('/zips/<zip_id>')
 def zip(zip_id):
-	return render_template('zips1.html', zip_id=zip_id)
+	return app.send_static_file('zips1.html', zip_id=zip_id)
 
 
 if __name__ == "__main__":
