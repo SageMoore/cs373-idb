5d3007ab496e863e729beb9fbd1546d09966397a
SageMoore <sagemoore@utexas.edu>
2015-11-03 22:12:18 -0600

    ready to add all of the data

--- stuff.py
+++ stuff.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import datetime
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
@@ -28,6 +29,9 @@ def add():
     zip_1 = Zip(zipcode=78704, lat=32.123, lng=32.123, pop=5)
     zip_2 = Zip(zipcode=78705, lat=30.123, lng=30.123, pop=5)
     zip_3 = Zip(zipcode=78706, lat=35.123, lng=35.123, pop=5)
+    week_1 = Week(start=datetime.date(year=2015, month=10, day=11), end=datetime.date(year=2015, month=10, day=17))
+    week_2 = Week(start=datetime.date(year=2015, month=10, day=18), end=datetime.date(year=2015, month=10, day=24))
+    week_3 = Week(start=datetime.date(year=2015, month=10, day=25), end=datetime.date(year=2015, month=10, day=31))
     try:
         session.add(crime_1)
         session.add(crime_2)
@@ -38,6 +42,9 @@ def add():
         session.add(zip_1)
         session.add(zip_2)
         session.add(zip_3)
+        session.add(week_1)
+        session.add(week_2)
+        session.add(week_3)
         print("commiting to database")
         session.commit()
     except:
@@ -52,4 +59,4 @@ def read():
     print(str(person[11].name))
     session.close()
 
-read()
+add()
