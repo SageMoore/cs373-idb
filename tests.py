d6032bab172a594b7e62d2cf9b50862287e6e8b3
dustinstone <stone.dustin94@gmail.com>
2015-11-04 21:23:17 -0600

    Editing tests

--- tests.py
+++ tests.py
@@ -223,36 +223,36 @@ class CrimecastAPITestCase(unittest.TestCase):
     def test_crimes_has_id(self):
         rv = self.app.get('/api/v1/crimes/1')
         data = json.loads(rv.data)
-        self.assert_equal(data["id"], "1")
+        assert data["id"] == "1"
 
     def test_crimes_has_address(self):
         rv = self.app.get('/api/v1/crimes/1')
         data = json.loads(rv.data)
-        self.assert_equal(data["address"], "GDC")
+        assert data["address"] == "GDC"
 
     def test_crimes_has_type(self):
         rv = self.app.get('/api/v1/crimes/1')
         data = json.loads(rv.data)
-        self.assert_equal(data["crime_type"], {
+        assert data["crime_type"] == {
                 'crime_type_id': '1',
                 'name': 'Vandalism'
-             })
+             }
 
     def test_crimes_has_zip(self):
         rv = self.app.get('/api/v1/crimes/1')
         data = json.loads(rv.data)
-        self.assert_equal(data["zip_code"], {
+        assert data["zip_code"] == {
                 'zip_id': '1',
                 'zip_code': '78704'
-            })
+            }
 
     def test_crimes_has_week(self):
         rv = self.app.get('/api/v1/crimes/1')
         data = json.loads(rv.data)
-        self.assert_equal(data["week"], {
+        assert data["week"], {
                 'week_id': '1',
                 'start_date': '10/11/15'
-            })
+            }
 
     # ----------------------
     # Crime_Types unit tests
