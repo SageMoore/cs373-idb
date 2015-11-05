54de91936061c5d70cec4580bf9b54d2df99a71a
dustinstone <stone.dustin94@gmail.com>
2015-11-04 20:51:33 -0600

    Trying other test harness

--- crimecast.py
+++ crimecast.py
@@ -292,10 +292,26 @@ class ZipById(Resource):
 class Tests(Resource):
     def get(self):
         #p = subprocess.Popen('python cs373-idb/tests.py', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        p = subprocess.Popen('echo "Test process HERP DERP FLERP"', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output, errors = p.communicate()
-        return { 'results': str(output) }
+        #p = subprocess.Popen('echo "Test process HERP DERP FLERP"', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #output, errors = p.communicate()
+        #return { 'results': str(output) }
         #return { 'results': 'HERP DERP FLERP' }
+
+        res = ''
+        path = os.path.dirname(os.path.realpath(__file__))
+        for i in run_command(('python3 ' + path + '/tests.py').split()):
+            res += i.decode("utf-8")
+
+        return Response(res, mimetype='text/plain')
+
+def run_command(exe):
+    p = subprocess.Popen(exe, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    while True:
+        retcode = p.poll()  # returns None while subprocess is running
+        line = p.stdout.readline()
+        yield line
+        if retcode is not None:
+            break
 
 ##
 ## Actually setup the Api resource routing here
