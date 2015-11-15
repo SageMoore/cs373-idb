5bbee638ba990ec25fe1a7fb05c62c7c986f8b4a
Mark Daniel <mark.r.daniel@gmail.com>
2015-11-15 17:54:17 -0600

    adding more crimes

--- stuff.py
+++ stuff.py
@@ -103,8 +103,8 @@ def transform_data(data, count, sampling_index):
 
 # Insert everything into the crimedata database
 def add():
-    count = 700
-    sampling_index = 7
+    count = 1500
+    sampling_index = 14
     with open("extraction/daily_spot_crime_data.json") as data_file:
         data = json.load(data_file)
     transform_data(data, count, sampling_index)
@@ -220,7 +220,7 @@ def add_all_crime_types_to_crimes(data, count):
         print("Everything broke")
 
 def add_crime_type_to_crimes():
-    count = 700
+    count = 1400
     with open("extraction/daily_spot_crime_data.json") as data_file:
         data = json.load(data_file)
     add_all_crime_types_to_crimes(data, count)
@@ -316,7 +316,7 @@ def print_everything():
     zips = session.query(Zip).all()
     print("zips len: " + str(len(zips)))
 
-# add()
+add()
 #add_weeks_to_crimes()
 #add_zips_to_crimes()
 add_crime_type_to_crimes()
