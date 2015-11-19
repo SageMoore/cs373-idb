935ed6ee1136b17a6769b00110e2ff840675d5c3
Mark Daniel <mark.r.daniel@gmail.com>
2015-11-19 00:15:35 -0600

    adding cars

--- crimecast.py
+++ crimecast.py
@@ -292,7 +292,7 @@ api.add_resource(WeekById, '/api/v1/weeks/<week_id>')
 api.add_resource(ZipList, '/api/v1/zips')
 api.add_resource(ZipById, '/api/v1/zips/<zip_id>')
 api.add_resource(Tests, '/api/v1/tests')
-api.add_resource(Cars, 'api/v1/cars')
+api.add_resource(Cars, '/api/v1/cars')
 api.init_app(app)
 
 
