559967eef9bd34312d64151eb8981958e0eda3c9
chance raine <chanceraine@gmail.com>
2015-11-18 21:27:49 -0600

    added markers for zips

--- static/angular/controllers.js
+++ static/angular/controllers.js
@@ -266,9 +266,9 @@ crimeCastApp.controller('crimeCastCtrl', function($scope, $state, $stateParams, 
                 }
             });
             console.log('data for zips is...: ', data);
-        })
-        angular.forEach($scope.zips, function(value, key) {
+            angular.forEach($scope.zips, function(value, key) {
                 services.addMarker(value.lat, value.lng, value.pop, map, value.family_income);
+            })
         })
     };
 
