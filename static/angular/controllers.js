81c4dcd47bf7af0f869c2faefe1c85d1eabb4d59
Mark Daniel <mark.r.daniel@gmail.com>
2015-11-18 19:48:48 -0600

    adding cars

--- static/angular/controllers.js
+++ static/angular/controllers.js
@@ -293,5 +293,19 @@ crimeCastApp.controller('crimeCastCtrl', function($scope, $state, $stateParams, 
         })
     };
 }).controller('carCtrl', function ($scope, http_service) {
-    
+    var map = services.getMap();
+
+    var loadAllWidgets = function() {
+        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+    };
+
+    var destroyAllWidgets = function() {
+        var $ = function (id) { return document.getElementById(id); };
+        var twitter = $('twitter-wjs');
+        if (twitter != null)
+            twitter.remove();
+    };
+
+    destroyAllWidgets();
+    loadAllWidgets();
 });
