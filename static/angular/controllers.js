57a41a30299fbb6a74ad03abac9fe8e73ad387e6
dustinstone <stone.dustin94@gmail.com>
2015-11-18 20:41:29 -0600

    Searching

--- static/angular/controllers.js
+++ static/angular/controllers.js
@@ -362,7 +362,7 @@ crimeCastApp.controller('crimeCastCtrl', function($scope, $state, $stateParams, 
         })
     }   
 
-    $scope.crime_types = getCrimeTypes();
+    $scope.crime_types = getCrimesTypes();
     $scope.zips = getZips();
     $scope.crimes = getCrimes(); 
 
